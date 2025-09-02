d639a7d6d93cc808e2a5a5e997d0c59480c234b9
Maxence GAGNANT <maxence.gagnant@irt-systemx.fr>
2025-09-02 11:57:00 +0200

    Fix format of llm output for open AI LLM

--- ontoconnectlm/information_extraction/information_extractor.py
+++ ontoconnectlm/information_extraction/information_extractor.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from langchain_core.language_models.llms import BaseLLM
+from langchain_core.messages.ai import AIMessage
 import json
 
 # Abstract class for information extraction classes
@@ -14,12 +15,17 @@ class InformationExtractor(ABC):
 
     @staticmethod
     def decode_result(json_result : str) -> str:
+
+        # Case using OpenAI LLM
+        if isinstance(json_result, AIMessage):
+            json_result = json_result.content
+
         try:
             result = json_result.split("```")[1]
             result = result.strip("json")
             return json.loads(result)
         except Exception as E:
-            print("Erreur de décodage des résultats : " , json_result)
+            print("Erreur extracting result from json structured output : " , json_result)
             print(E)
             return json_result
         
