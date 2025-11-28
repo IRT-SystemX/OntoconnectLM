932dd29b09ec702f86c385f907a0084a17b0644b
Eamdouni <emna.amdouni@irt-systemx.fr>
2025-11-28 19:26:21 +0100

    [FIX] Fix ontoconnectlm/ontology_enrichment/onto_updater.py and ontoconnectlm/ontology_enrichment/wikidata_entity_linking.py

--- ontoconnectlm/ontology_enrichment/wikidata_entity_linking.py
+++ ontoconnectlm/ontology_enrichment/wikidata_entity_linking.py
@@ -4,12 +4,12 @@ from owlready2 import get_ontology
 from nerd import nerd_client
 from SPARQLWrapper import SPARQLWrapper, JSON
 import tempfile
-
+ 
 WIKIDATA_QUERY_FILEPATH = "sparql_queries/wikidata_query.txt"
 WIKIDATA_ENDPOINT = "https://query.wikidata.org/sparql"
-
+ 
 class Wikidata_linking:
-
+ 
     """
     Service aiming to extract entity metadata from Wikidata
     Input: A populated ontology
@@ -20,34 +20,32 @@ class Wikidata_linking:
     * Export the results to an Excel file
     Output: An Excel file containing all the collected information from Wikidata
     """
-       
     def __init__(self,):
-
+ 
         self.nerd = nerd_client.NerdClient()
-
+ 
         # Loading wikidata query
         load_path = os.path.join(os.path.dirname(__file__), WIKIDATA_QUERY_FILEPATH)
         with open(load_path, "r") as read_file:
             self.wikidata_query = read_file.read()
-
+ 
         self.wikidata_endpoint = WIKIDATA_ENDPOINT
-        
 
+ 
     def extract_wikidata_metadata(self, resource):
-
+ 
         query = self.wikidata_query.format(resource=resource)
-
+ 
         sparql = SPARQLWrapper(self.wikidata_endpoint)
         sparql.setQuery(query)
         sparql.setReturnFormat(JSON)
-
+ 
         try:
             results = sparql.query().convert()
             data ={'wikimetadata': []}
-
+ 
             for result in results['results']['bindings']:
                     entry={}
-                    
                     if 'label' in result:
                             entry['label']=result['label']['value']
                     if 'instanceof' in result: 
@@ -58,39 +56,35 @@ class Wikidata_linking:
                             entry['image']=result['image']['value']
                     if 'googleid' in result:
                             entry['googleid']=result['googleid']['value']
-                    
                     data['wikimetadata'].append(entry)
-                    
             return data    
-            
         except Exception as e:
                 print("Une erreur s'est produite :", str(e))
         return None
-
+ 
     
     def process_individuals(self, cls):
         results = []
         for individual in cls.instances():
             value = str(individual.has_text)
-
+ 
             if value is not None and value != "":
                 try:
                     result = self.nerd.disambiguate_text(f" {value} ", language="fr")
                     entities = result[0].get('entities')
-
+ 
                     for ent in entities:
-    
                         label = ""
                         description = ""
                         instanceOfs = []
                         images = []
-
+ 
                         rawName = ent.get('rawName', 'N/A')
                         wiki_id = ent.get('wikidataId', 'N/A')
                         confidence = ent.get('confidence_score', 'N/A')
-
+ 
                         data = self.extract_wikidata_metadata(wiki_id)
-
+ 
                         for item in data.get('wikimetadata', []):
                             if 'label' in item and item['label'] is not None:
                                 label = item.get('label', 'N/A')
@@ -101,7 +95,7 @@ class Wikidata_linking:
                                 individual.seeAlso.extend(instanceOfs)
                             if 'image' in item and item['image'] is not None:
                                 images.append(item.get('image', 'N/A'))
-
+ 
                         results.append({
                             'Raw text': value,
                             'Spot': rawName,
@@ -116,24 +110,25 @@ class Wikidata_linking:
                     print(f"print{e}")
             else:
                 print(f"Value is None or empty for individual {individual}. Skipping this one.")
-            
         return results
-
+ 
     
     def wikidata_enrichment(self, ontology_content : str) -> pd.DataFrame:
-
+ 
         enrichment_results = []
-
+ 
         with tempfile.NamedTemporaryFile(delete=True, suffix='.owl', mode="w") as tmp_file:
             tmp_file.write(ontology_content)
             ontology = get_ontology(tmp_file.name).load()
-
+ 
         for ontology_class in ontology.classes():
             results = self.process_individuals(ontology_class)
             if results:
                 df = pd.DataFrame(results)
                 enrichment_results.append(df)
-
-        return pd.concat(enrichment_results)
-
-
+ 
+ 
+            if enrichment_results:
+                return pd.concat(enrichment_results)
+            else:
+                return pd.DataFrame()
